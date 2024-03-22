28fd656c272e1bfcc9ddc53023ddf769d70b6858
zhabwe254 <zhabwe@gmail.com>
2024-03-22 22:46:10 +0300

    a

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-# installs puppet-lint package
-
-package {'flask':
-  ensure   => '2.1.0',
-  name     => 'flask',
-  provider => 'pip3'
-}
+# install flask from pip3
+package { 'flask' :
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    }
