9df72738045fb9b70bdb2cbda8f47dc2285c3f36
zhabwe254 <zhabwe@gmail.com>
2024-03-22 22:33:10 +0300

    a

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,20 @@
 # Puppet manifest to install flask from pip3
+class { 'python':
+  version => '3.8',
+}
+
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+exec { 'upgrade_pip':
+  command => '/usr/bin/python3 -m pip install --upgrade pip',
+  path    => ['/bin', '/usr/bin'],
+  unless  => '/usr/bin/python3 -m pip --version | grep -qF "20.3"',
+}
+
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Exec['upgrade_pip'],
 }
