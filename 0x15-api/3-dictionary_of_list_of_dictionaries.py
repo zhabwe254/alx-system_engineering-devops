138ff13e4d215c0be307d7917088ea754d21e9e8
zhabwe254 <zhabwe@gmail.com>
2024-04-22 22:20:08 +0300

    a

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,12 +7,12 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     users = requests.get(url + "users").json()
 
-    all_todos = {}
-    for user in users:
-        user_id = user.get("id")
-        username = user.get("username")
-        todos = requests.get(url + "todos", params={"userId": user_id}).json()
-        all_todos[user_id] = [{"task": t.get("title"), "completed": t.get("completed"), "username": username} for t in todos]
-
     with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump(all_todos, jsonfile, indent=4)
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
